d224a9d49c9b67a54635f7bd7612fbb713c9b02d
Med <mehdi.gacem@versusmind.eu>
2018-12-04 17:07:47 +0100

    change the organisation of the code to structure the Car service by method

--- app/services/services.js
+++ app/services/services.js
@@ -15,12 +15,28 @@ carService.constant('apiURL',
 carService.factory("Car", ["$resource", "apiURL", function($resource, apiURL)
     {
         console.log(apiURL);
-        return $resource(
-                            apiURL.baseURL+apiURL.carListEndpoint, 
-                            {}, 
-                            {
-                                carList : {methods : "GET", params : "", isArray : true} 
-                            }
-                        );
+        this.carList = function carList()
+        {
+            // console.log($resource);
+            return $resource(apiURL.baseURL+apiURL.carListEndpoint, 
+                      {}, 
+                      {
+                        carList : {methods : "GET", params : {}, isArray : true} 
+                      }
+                     );
+        }
+        
+        this.carSpecific = function carSpecific(id)
+        {
+            console.log("Fetch that car : "+id);
+            return $resource(apiURL.baseURL+apiURL.carSpecificEndpoint, 
+                      {carId : "@id"}, 
+                      {
+                        carSpecific : {methods : "GET", params : {}, isArray : false} 
+                      }
+                     );
+        }
+        
+        return this;
     }
 ]);
