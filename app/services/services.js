5f4975ba335ff12e07cd8c0373abe8d1daab52db
Med <mehdi.gacem@versusmind.eu>
2018-12-06 10:27:25 +0100

    add service function to insert a car to the webservice

--- app/services/services.js
+++ app/services/services.js
@@ -17,26 +17,39 @@ carService.factory("Car", ["$resource", "apiURL", function($resource, apiURL)
         console.log(apiURL);
         this.carList = function carList()
         {
-            // console.log($resource);
+            console.log("Car List service method");
             return $resource(apiURL.baseURL+apiURL.carListEndpoint, 
-                      {}, 
-                      {
-                        carList : {methods : "GET", params : {}, isArray : true} 
-                      }
-                     );
+                {}, 
+                {
+                    carList : {methods : "GET", params : {}, isArray : true} 
+                }
+                );
+            }
+            
+            this.carSpecific = function carSpecific(id)
+            {
+                console.log("Car Specific service method");
+                console.log("Fetch that car : "+id);
+                return $resource(apiURL.baseURL+apiURL.carSpecificEndpoint, 
+                        {carId : "@id"}, 
+                        {
+                            carSpecific : {methods : "GET", params : {}, isArray : false} 
+                        }
+                        );
         }
         
-        this.carSpecific = function carSpecific(id)
+        this.postCar = function postCar(car)
         {
-            console.log("Fetch that car : "+id);
-            return $resource(apiURL.baseURL+apiURL.carSpecificEndpoint, 
-                      {carId : "@id"}, 
+            console.log("Car to create : "+angular.toJson(car));
+            return $resource(apiURL.baseURL+apiURL.carCreateEndpoint, 
+                      {}, //{carId : "@id"}
                       {
-                        carSpecific : {methods : "GET", params : {}, isArray : false} 
+                        carCreate : {methods : "POST", params : {}, isArray : true, } //headers : {'Content-Type': 'application/x-www-form-urlencoded'}
                       }
                      );
         }
         
+
         return this;
     }
 ]);
