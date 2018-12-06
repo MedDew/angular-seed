3dc72db8f1e036c9a32a58c22fcfea5d65073345
Med <mehdi.gacem@versusmind.eu>
2018-12-06 10:25:12 +0100

    create t2  function to submit and reset a car form when we arre on the corresponding route + modify conditionnal structure to mette the route criteria + change the configuration order of the route

--- app/car/car.js
+++ app/car/car.js
@@ -6,12 +6,13 @@ carModule.constant("redirectedRoute", "http://localhost:8000/#!/cars");
 var carConfig = carModule.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
     $locationProvider.hashPrefix('');
     $routeProvider.when("/cars", { templateUrl : "car/carView.html", controller : "CarController"})
+    .when("/cars/create", { templateUrl : "car/carPostView.html", controller : "CarController"})
     .when("/cars/:id", { templateUrl : "car/carSpecificView.html", controller : "CarController"})
 }]);
 
 carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirectedRoute", "$routeParams", function($scope, Car, $location, redirectedRoute, $routeParams){
     
-    if($routeParams.id === undefined)
+    if($routeParams.id === undefined && $location.path() == "/cars")
     {
         $scope.cars = Car.carList().query();
         console.log(Car);
@@ -33,7 +34,7 @@ carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirecte
         }
     }
     
-    if($routeParams.id)
+    if($routeParams.id && $routeParams.id.match(/\d+/))
     {
         console.log("Param Id : "+$routeParams.id);
         console.log("Param Id is undefined : ");
@@ -41,5 +42,53 @@ carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirecte
         $scope.specificCar = Car.carSpecific().get({carId : $routeParams.id}, function(data){
             console.log("DATA : "+angular.toJson(data));
         });
+    }
+
+    console.log("Is /cars/create path : ");
+    console.log($location.path() == "/cars/create");
+    console.log($routeParams);
+    if($routeParams.id === undefined && $location.path() == "/cars/create")
+    {
+        $scope.carFormValues = {};
+        console.log("/cars/create");
+        $scope.submitCarCreateValues = function(car){
+            console.log("YAWP");
+            if($scope.carCreate.$valid)
+            {
+                console.log("VALID POSTED CAR FORM VALUES : ");
+                console.log($scope.carCreate.$$controls);
+                $scope.carFormValues = angular.copy(car);
+                console.log("DATA TO POST");
+                console.log($scope.carFormValues);
+                var test = Car.postCar().save($scope.carFormValues);;//.carCreate().$promise.then(function(data){
+                    // console.log("Created car : "+angular.toJson(car));
+                    for(var p in test)
+                        console.log("p :::: "+p);
+                //});
+            }
+            else
+            {
+                console.log("IS FORM VALID : "+$scope.carCreate.$valid);
+            }
+        }
+
+        $scope.resetCarCreateForm = function(form){
+            console.log("CAR FORM VALUES in resetCarCreateForm function: ");
+            console.log($scope.car);
+            console.log(form);
+            console.log(form.$submitted);
+            if(form)
+            {
+                form.$setPristine();
+                form.$setUntouched();
+            }
+            console.log(form.$submitted);
+            
+            
+            //Allow to reset form after submission
+            $scope.carFormValues = {};
+            $scope.car = angular.copy($scope.carFormValues);
+            console.log($scope.car);
+        }
     }
 }]);
