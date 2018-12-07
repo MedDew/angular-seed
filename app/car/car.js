2a02626c64cdf5769de4dbac53c6c4c33e2f61e7
Med <mehdi.gacem@versusmind.eu>
2018-12-07 10:26:10 +0100

    add url for updating a car + implement the logic to update a car according to the expected url

--- app/car/car.js
+++ app/car/car.js
@@ -8,6 +8,7 @@ var carConfig = carModule.config(["$routeProvider", "$locationProvider", functio
     $routeProvider.when("/cars", { templateUrl : "car/carView.html", controller : "CarController"})
     .when("/cars/create", { templateUrl : "car/carPostView.html", controller : "CarController"})
     .when("/cars/:id", { templateUrl : "car/carSpecificView.html", controller : "CarController"})
+    .when("/cars/update/:carId", { templateUrl : "car/carUpdateView.html", controller : "CarController"})
 }]);
 
 carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirectedRoute", "$routeParams", function($scope, Car, $location, redirectedRoute, $routeParams){
@@ -42,13 +43,15 @@ carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirecte
         $scope.specificCar = Car.carSpecific().get({carId : $routeParams.id}, function(data){
             console.log("DATA : "+angular.toJson(data));
         });
+
+        console.log("DATA specificCar: ", $scope.specificCar);
     }
 
-    console.log("Is /cars/create path : ");
-    console.log($location.path() == "/cars/create");
-    console.log($routeParams);
     if($routeParams.id === undefined && $location.path() == "/cars/create")
     {
+        console.log("Is /cars/create path : ");
+        console.log($location.path() == "/cars/create");
+        console.log($routeParams);
         $scope.carFormValues = {};
         console.log("/cars/create");
         $scope.submitCarCreateValues = function(car){
@@ -60,7 +63,7 @@ carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirecte
                 $scope.carFormValues = angular.copy(car);
                 console.log("DATA TO POST");
                 console.log($scope.carFormValues);
-                var test = Car.postCar().save($scope.carFormValues);;//.carCreate().$promise.then(function(data){
+                var test = Car.postCar().save($scope.carFormValues);//.carCreate().$promise.then(function(data){
                     // console.log("Created car : "+angular.toJson(car));
                     for(var p in test)
                         console.log("p :::: "+p);
@@ -90,5 +93,36 @@ carConfig.controller("CarController", ["$scope", "Car" , "$location", "redirecte
             $scope.car = angular.copy($scope.carFormValues);
             console.log($scope.car);
         }
+    }
+    
+    if($routeParams.carId && /\/cars\/update\/\d+/.test($location.path()))
+    {
+        console.log("Is car update route");
+        console.log(/\/cars\/update\/\d+/.test($location.path()));
+
+        console.log("$scope : "+angular.toJson($scope));
+        // for(var p in Car)
+        //     console.log("p : "+p);
+
+        
+        $scope.updateCarSpecifc = Car.carSpecific({carId : $routeParams.carId}).get({carId : $routeParams.carId}, function(data){
+            console.log("Found car to update : "+angular.toJson(data));
+            //$scope.updateCarSpecifc = data;
+        });//.$promise.then(function(response) {
+        //     $scope.updateCarSpecifcTwo = response;
+        //     console.log("Reponse", $scope.updateCarSpecifcTwo)
+        // });
+        
+        console.log("Reponse2", $scope.updateCarSpecifcTwo)
+        // console.log(updateCarSpecifc)
+        console.log("HEY : " , $scope.updateCarSpecifc);
+
+        $scope.submitCarUpdateValues = function(updatedCar){
+            console.log("Updated car : ", updatedCar, updatedCar.id);
+            let car = Car.putCar().carUpdate({carId : updatedCar.id}, updatedCar);
+            console.log("Car updated : " , car);
+            for(var p in Car)
+                console.log("p : " + p);
+        };
     }
 }]);
