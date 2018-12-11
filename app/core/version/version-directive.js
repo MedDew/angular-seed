639528a048036cd40949f5f16a29f8b6055b434a
Med <mehdi.gacem@versusmind.eu>
2018-12-11 13:00:56 +0100

    add some trace about paremeters pass to function and directive

--- app/core/version/version-directive.js
+++ app/core/version/version-directive.js
@@ -4,6 +4,10 @@ angular.module('myApp.version.version-directive', [])
 
 .directive('appVersion', ['version', function(version) {
   return function(scope, elm, attrs) {
+    console.log("scope : ", scope);
+    console.log("elm : ", elm);
+    console.log("attrs : ", attrs);
+    console.log("version : ", version);
     elm.text(version);
   };
 }]);
